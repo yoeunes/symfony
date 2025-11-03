bc5cc8cc8ae7687d7f62bad2417263421c51d17a
Younes ENNAJI <younes.ennaji.pro@gmail.com>
2025-11-03 01:27:25 +0100

    [Filesystem] Unify logic for isAbsolute() in Path

--- src/Symfony/Component/Filesystem/Filesystem.php
+++ src/Symfony/Component/Filesystem/Filesystem.php
@@ -580,13 +580,7 @@ class Filesystem
      */
     public function isAbsolutePath(string $file): bool
     {
-        return '' !== $file && (strspn($file, '/\\', 0, 1)
-            || (\strlen($file) > 3 && ctype_alpha($file[0])
-                && ':' === $file[1]
-                && strspn($file, '/\\', 2, 1)
-            )
-            || null !== parse_url($file, \PHP_URL_SCHEME)
-        );
+        return Path::isAbsolute($file);
     }
 
     /**
